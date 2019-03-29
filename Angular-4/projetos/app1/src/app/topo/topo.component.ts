4f273e23d31a8570d22f2a3c1f27aa2cbf9c46f0
leonardoigor <igor_mendonca@Outlook.com.br>
2019-03-29 02:24:56 -0300

    angulas-styles

---
+++ Angular-4/projetos/app1/src/app/topo/topo.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: '[app-topo]',
+    templateUrl:'./topo.component.html',
+    // template: `<p>
+    // top componente!
+    // </p>`
+    // styles:['.exemplo{color:red']
+    styleUrls:['./topo.component.css']
+})
+export class TopoComponent {
+
+}

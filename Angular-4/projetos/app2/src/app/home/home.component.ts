efbc26fb81794ece3bdf7eabda401ca5e5d965f0
leonardoigor <igor_mendonca@Outlook.com.br>
2019-04-01 00:36:40 -0300

    res

--- Angular-4/projetos/app2/src/app/home/home.component.ts
+++ Angular-4/projetos/app2/src/app/home/home.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
+  public ofertas:Array<Oferta>
+  
+ 
+  constructor(private ofertasServices:OfertasService) {
+    
+    
+   }
   ngOnInit() {
+    // this.ofertas = this.ofertasServices.getOfertas()
+    // console.log(this.ofertas)
+    
+    this.ofertasServices.getOfertas2()
+    .then((ofertas:Oferta[])=>{
+      console.log('3sec')
+      this.ofertas = ofertas
+    })
+    .catch((param:any)=>console.log(param))
   }
-
+  
+  
 }
